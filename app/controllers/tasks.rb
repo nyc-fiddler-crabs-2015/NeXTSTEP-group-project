e791e48d69b5af2cdc4f795bdad106a6f7c428f6
Vivek Ratkalkar <vratkalkar@live.com>
2015-02-28 18:01:31 -0500

    Add task routes

--- app/controllers/tasks.rb
+++ app/controllers/tasks.rb
@@ -9,3 +9,24 @@ post '/task/create' do
   Task.create(title: params[:title], completed: false, day: params[:day], user_id: user.id , category_id: params[:category])
   redirect '/users/#{user.id}'
 end
+
+get '/task/:id' do
+ @task = Task.find(params[:id])
+ erb :'task/show'
+end
+
+get '/task/:id/edit' do
+  @task = Task.find(params[:id])
+  erb :'task/edit'
+end
+
+put '/task/:id' do
+  @task = Task.find(params[:id])
+  @task.update_attributes(params[:title])
+  redirect '/task/#{id}'
+end
+
+delete '/task/:id' do
+  @task = Task.find(params[:id]).destroy
+  redirect '/users/<%= user.id %>'
+end
