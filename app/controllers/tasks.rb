cc96f4899c32787599eaf18926194434708300ee
Natalia Bryzhatenko <notika314@gmail.com>
2015-02-28 16:12:13 -0500

    fix typo in tasks.rb

--- app/controllers/tasks.rb
+++ app/controllers/tasks.rb
@@ -1,11 +1,11 @@
 get '/task/new' do
-  @day = parms[:day]
+  @day = params[:day]
   @category = Category.all
   erb :'/task/create'
 end
 
-post '/task' do
+post '/task/create' do
   user = User.find(session[:user_id])
-  Task.create(title: params[:title], completed: false, day: params[:day], user: user.id , caegory: params[:category])
-  redirect '/users/:id'
+  Task.create(title: params[:title], completed: false, day: params[:day], user: user.id , category: params[:category])
+  redirect '/users/#{user.id}'
 end
