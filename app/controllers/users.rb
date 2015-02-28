8d4f25c1a3f2a96388ed3ef122d3f8a746fcaf4e
Trevor Nelson <trevor.nelson1@gmail.com>
2015-02-28 17:21:12 -0500

    Move tasks data structure generation functionality to User model

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,8 +1,6 @@
 get '/users/:id' do
   @user = User.find(params[:id])
   @tasks = {}
-  ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Weekend'].each do |day|
-    @tasks[day] = @user.tasks.where(day: day)
-  end
+
   erb :'users/show'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tasks
+
+  def fetch_tasks
+    output_tasks = {}
+    ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Weekend'].each do |day|
+      output_tasks[day] = {open: @user.tasks.where(day: day, completed: false),
+                           complete: @user.tasks.where(day: day, completed: true)}
+    end
+    output_tasks
+  end
 end
