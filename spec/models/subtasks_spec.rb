b069440a42b6d0da0f5de1f62f8e0d9b02e810ae
Vivek Ratkalkar <vratkalkar@live.com>
2015-03-01 16:46:26 -0500

    fixed typo causing test failure

--- spec/models/subtasks_spec.rb
+++ spec/models/subtasks_spec.rb
@@ -2,14 +2,14 @@ require_relative '../spec_helper'
 
 describe 'Subtasks Model' do
   it 'marked the subtask as completed in the model object' do
-    task = task.create(title: 'test task', day: 'Monday', user_id: 1, category_id: 1)
+    task = Task.create(title: 'test task', day: 'Monday', user_id: 1, category_id: 1)
     subtask = Subtask.new(title: 'This subtask should be complete', task_id: task.id)
     subtask.completed = true
     expect(subtask.completed).to eq(true)
   end
 
   it 'marked the task as completed in the database' do
-    task = task.create(title: 'test task', day: 'Monday', user_id: 1, category_id: 1)
+    task = Task.create(title: 'test task', day: 'Monday', user_id: 1, category_id: 1)
     subtask = Subtask.create(title: 'This subtask should be complete', task_id: task.id)
     subtask.completed = true
     subtask.save
