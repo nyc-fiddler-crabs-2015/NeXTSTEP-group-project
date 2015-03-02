2ceee705682421d21483143d04e7d1b216544559
Notika314 <notika314@gmail.com>
2015-03-01 20:54:39 -0500

    style edits, add mre cats to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
 User.create(first_name: "Kiran", last_name: "C", email: "phanendar@gmail.com", password: "1234")
 User.create(first_name: "Trevor", last_name: "Nelson", email: "trevor@gmail.com", password: "1234")
 Category.create(title: "recreation")
-Category.create(title: "family")
+User.create(first_name: "Natalia", last_name: "Bryzhatenko", email: "natalia@gmail.com", password: "1001")
+Category.create(title: "family/relationships")
+Category.create(title: "learning")
+Category.create(title: "health")
